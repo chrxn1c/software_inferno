fd90aae2a6c9f991fce94d9ce2d1a5c1e7b76b3f
chrxn1c <rivalofficialentertainment@gmail.com>
2024-05-24 15:20:55 +0300

    front working

--- api/gunicorn.conf.py
+++ api/gunicorn.conf.py
@@ -1,6 +1,4 @@
-from os import getenv
-
-bind = f"{getenv(key="HOST", default="127.0.0.1")}:{getenv(key="PORT", default=8000)}"
+bind = "0.0.0.0:8000"
 wsgi_app = "general.wsgi:application"
-workers = getenv(key="WORKERS", default=1)
-threads = getenv(key="THREADS", default=1)
+workers = 4
+threads = 2
